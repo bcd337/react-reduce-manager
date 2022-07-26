83d07f5b924633005941153fe23f2e1f0adb81c2
iqbal <b.bcd337@gmail.com>
2022-07-26 16:25:22 +0700

    add dummy test

---
+++ src/useReducerManager.test.ts
@@ -0,0 +1,7 @@
+import useReducerManager from "./useReducerManager"
+
+describe('useReducerManager', () => {
+  test('is useReducerManager exist', () => {
+    expect(useReducerManager).toBeTruthy()
+  })
+})
